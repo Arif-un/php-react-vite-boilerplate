d16ea1236d6a6d6f57cd2100eb888ec3598a7337
Arif Uddin <arifunctg@gmail.com>
2022-08-23 11:38:19 +0600

    drop cross env use vite mode instead

--- vite.config.js
+++ vite.config.js
@@ -3,42 +3,44 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  return {
 
-  plugins: [
-    react(),
-    reactRefresh(),
-  ],
+    plugins: [
+      react(),
+      reactRefresh(),
+    ],
 
-  // config
-  root: 'src',
-  base: process.env.APP_ENV === 'development' ? '/php-react-vite-boilerplate/src/' : '/dist/',
+    // config
+    root: 'src',
+    base: mode === 'development' ? '/php-react-vite-boilerplate/src/' : '/dist/',
 
-  build: {
-    // output dir for production build
-    outDir: '../public/dist',
-    emptyOutDir: true,
+    build: {
+      // output dir for production build
+      outDir: '../public/dist',
+      emptyOutDir: true,
 
-    // emit manifest so PHP can find the hashed files
-    manifest: true,
+      // emit manifest so PHP can find the hashed files
+      manifest: true,
 
-    target: 'es2015',
+      target: 'es2015',
 
-    // our main entry
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.jsx'),
-    }
-  },
+      // our main entry
+      rollupOptions: {
+        input: path.resolve(__dirname, 'src/index.jsx'),
+      }
+    },
 
-  server: {
-    // required to load scripts from custom host
-    cors: true,
+    server: {
+      // required to load scripts from custom host
+      cors: true,
 
-    // we need a strict port to match on PHP side
-    strictPort: true,
-    port: 3000,
-    hmr: {
-      host: 'localhost',
-    }
-  },
+      // we need a strict port to match on PHP side
+      strictPort: true,
+      port: 3000,
+      hmr: {
+        host: 'localhost',
+      }
+    },
+  }
 })
